b781aac6103fc1e2f21f47fe2ddb1b7cb789232d
Sahil Ahmed <ahmedsahilcr7@gmail.com>
2022-07-16 20:53:24 +0530

    qr added

--- js/scripts.js
+++ js/scripts.js
@@ -73,11 +73,3 @@ $('.carousel').owlCarousel({
         }
     }
 });
-
-const actualBtn = document.getElementById('actual-btn');
-
-const fileChosen = document.getElementById('file-chosen');
-
-actualBtn.addEventListener('change', function() {
-    fileChosen.textContent = this.files[0].name
-})
