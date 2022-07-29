e0a245ed15ccb794e43a9f8d5b0fac98d8440d33
Arghya Pal <arghya.1530@gmail.com>
2022-07-30 00:26:28 +0530

    logochange

--- razorpay/config1.php
+++ razorpay/config1.php
@@ -1,7 +1,7 @@
 <?php
 
-$keyId = 'rzp_test_oFhh12VRqHO4mX';
-$keySecret = 'PtTTOJhcP6ARsdgXwBqjIcNO';
+$keyId = 'rzp_test_C4CuhvpyjaRSrM';
+$keySecret = 'CbOyplKoA8JjAwr23VmAfIwT';
 $displayCurrency = 'INR';
 
 //These should be commented out in production
