5127b5e2565a2c9c2f2baf76feef3e6f2a638700
Arghya Pal <arghya.1530@gmail.com>
2022-08-05 23:19:39 +0530

    serg

--- team.php
+++ team.php
@@ -45,7 +45,7 @@
                 <!-- <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3> -->
             </div>
             <div class="row">
-                <!-- <div id="board" class="col-lg-4">
+                <div id="board" class="col-lg-4">
                     <div class="team-member">
                         <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="..." />
                         <h4>Sankha Das</h4>
@@ -110,36 +110,8 @@
                 <div class="col-lg-8 mx-auto text-center">
                     <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
                 </div>
-            </div> -->
+            </div>
 
-                <div class="card-list">
-                    <div class="card">
-                        <figure class="profile"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/584938/people_1.png" />
-                            <figcaption>
-                                <p class="name">Mel Moe</p>
-                                <p class="position">Head of software</p>
-                            </figcaption>
-                        </figure>
-                        <div class="body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero odio voluptates quibusdam excepturi amet, itaque aliquid, a sed atque obcaecati delectus eum saepe</div>
-                    </div>
-                    <div class="card">
-                        <figure class="profile"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/584938/people_2.png" />
-                            <figcaption>
-                                <p class="name">Jane Doe</p>
-                                <p class="position">Founder &amp; CEO</p>
-                            </figcaption>
-                        </figure>
-                        <div class="body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero odio voluptates quibusdam excepturi amet, itaque aliquid, a sed atque obcaecati delectus eum saepe</div>
-                    </div>
-                    <div class="card">
-                        <figure class="profile"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/584938/people_3.png" />
-                            <figcaption>
-                                <p class="name">Michelle Doe</p>
-                                <p class="position">Managing Partner</p>
-                            </figcaption>
-                        </figure>
-                        <div class="body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero odio voluptates quibusdam excepturi amet, itaque aliquid, a sed atque obcaecati delectus eum saepe</div>
-                    </div>
                 </div>
                 <div id="member">
                     <div class="text-center">
